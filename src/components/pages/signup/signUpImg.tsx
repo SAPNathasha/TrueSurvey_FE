b14d2d4d0498f3049be329ddabf4bffdfbd03fe6
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2026-05-19 16:02:02 +0530

    Create SignUpImg component

---
+++ src/components/pages/signup/signUpImg.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Image } from "@chakra-ui/react";
+
+export default function SignupImage() {
+  return (
+    <Image
+      height="380px"
+      width="100%"
+      objectFit="contain"
+      src="/signInImage.png"
+      alt="Sign up image"
+    />
+  );
+}
