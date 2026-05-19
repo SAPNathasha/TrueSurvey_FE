35b96de96dcf6a9fe7eb1ae5284ce762d7ec71aa
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2026-05-19 16:07:30 +0530

    Final verification of all the content, components and pages working properly

--- src/store/useSignupStore.ts
+++ src/store/useSignupStore.ts
@@ -1,17 +1,14 @@
 import { create } from "zustand";
 
-type UserRole = "participant" | "creator" | "both" | "" ;
+type UserRole = "participant" | "creator" | "both" | "";
 
 type SignupFormData = {
-  // Step 1 data
   fullName: string;
   email: string;
   password: string;
 
-  // Step 2 data
   role: UserRole;
 
-  // Step 3 data
   nicOrLicenseImage: File | null;
   selfieImage: File | null;
 };
